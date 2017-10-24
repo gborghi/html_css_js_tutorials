cccf0cf3a1c1bea647288ed25dfbb7c3add91e7b
Giovanni Borghi <gio.borghi@gmail.com>
2017-10-24 18:10:27 +0200

    Using beautify to fix code formatting in calculator

--- calculator/index.js
+++ calculator/index.js
@@ -1,21 +1,22 @@
 $("#button1").click(
-  function(test){
-    $(".prova").text("Perchè mi clicchi?");
-    return test;
-  }
+   function (test) {
+      $(".prova").text("Perchè mi clicchi?");
+      return test;
+   }
 );
 
-$( "#button2" ).click(
-  function(test){
-    $(".prova").text("Ciao"); return test;
-  }
+$("#button2").click(
+   function (test) {
+      $(".prova").text("Ciao");
+      return test;
+   }
 );
 
-$( "#somma" ).click(
-  function(test){
-    var num1=parseFloat($("#fname").val());
-    var num2=parseFloat($("#lname").val());
-    $("#sum").val(num1+num2);
-    return test;
-  }
+$("#somma").click(
+   function (test) {
+      var num1 = parseFloat($("#fname").val());
+      var num2 = parseFloat($("#lname").val());
+      $("#sum").val(num1 + num2);
+      return test;
+   }
 );
