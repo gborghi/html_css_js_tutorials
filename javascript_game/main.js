9c5c7f7c6b90c70173a003f7e454e395ee450223
Giovanni Borghi <gio.borghi@gmail.com>
2017-10-31 18:02:32 +0100

    added projects onemonthjquery

--- javascript_game/main.js
+++ javascript_game/main.js
@@ -4,7 +4,7 @@ var ctx = canvas.getContext('2d');
 
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
+ctx.fillStyle = "#478df7";
 ctx.fill();
 ctx.closePath();
 
@@ -19,3 +19,7 @@ ctx.rect(160, 10, 100, 40);
 ctx.strokeStyle = 'rgba(0,0,255,0.5)';
 ctx.stroke();
 ctx.closePath();
+
+function read() {
+
+}
