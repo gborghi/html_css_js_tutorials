86af73d74dca182171bcec6cb1194fc5fb801210
Giovanni Borghi <gio.borghi@gmail.com>
2017-11-01 15:46:12 +0100

    finished onemonthjquery/project3

--- onemonthjquery/project-3/index.js
+++ onemonthjquery/project-3/index.js
@@ -1,3 +1,34 @@
 $(document).ready(function () {
     // YOUR CODE HERE!
+   $("#list-items").html(localStorage.getItem("listItems"));
+
+   $(".add-items").submit(function(event){
+      event.preventDefault();
+      var item = $("#todo-list-item").val();
+      if (item) {
+         $("#list-items").append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>");
+         localStorage.setItem("listItems", $("#list-items").html());
+         $("#todo-list-item").val("");
+      }
+   });
+
+   //$(".checkbox").change(function() {
+
+   //})
+
+   $(this).on("change", ".checkbox", function() {
+      if ($(this).attr("checked")) {
+         $(this).removeAttr("checked");
+      } else {
+         $(this).attr("checked", "checked");
+      }
+      $(this).parent().toggleClass("completed");
+      localStorage.setItem("listItems", $("#list-items").html());
+   });
+
+   $(this).on("click", ".remove", function() {
+      $(this).parent().remove();
+      localStorage.setItem("listItems", $("#list-items").html());
+   })
+
 });
